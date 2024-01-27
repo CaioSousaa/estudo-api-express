76cdf5a9391d9e1fda0a1f48250e0d131e2a2324
Caio <caiothalles607@gmail.com>
2024-01-27 15:28:22 -0300

    fix(index-routes): added account routes to the main routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./userRoutes";
+import { accountRoutes } from "./accountRoutes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/accounts", accountRoutes);
 
 export { router };
