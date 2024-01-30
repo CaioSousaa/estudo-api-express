258d05ee48f4be0cb2b774ee6dd2aa1ffdfc5cef
Caio <caiothalles607@gmail.com>
2024-01-30 14:55:07 -0300

    fix(routes-accountsRoutes): add route for service account list

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreateAccountController } from "../modules/accounts/controllers/createAccountController";
+import { ListAccountsController } from "../modules/accounts/controllers/listAccountsController";
 
 const accountRoutes = Router();
 
 accountRoutes.post("/", (request, response) => {
   return new CreateAccountController().handle(request, response);
+});
+
+accountRoutes.get("/", (request, response) => {
+  return new ListAccountsController().handle(request, response);
 });
 
 export { accountRoutes };
