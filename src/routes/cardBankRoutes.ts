b966a77d2aab8199b7f2d672ea3cfb299ed69f2b
Caio <caiothalles607@gmail.com>
2024-01-31 20:39:05 -0300

    fix(cardBankRoutes): add route for list cards

--- src/routes/cardBankRoutes.ts
+++ src/routes/cardBankRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { CreateCardBankController } from "../modules/card_bank/controllers/createCardBankController";
+import { ListCardBankController } from "../modules/card_bank/controllers/listCardBankController";
 
 const cardBankRoutes = Router();
 
 cardBankRoutes.post("/", (request, response) => {
   return new CreateCardBankController().handle(request, response);
+});
+
+cardBankRoutes.get("/", (request, response) => {
+  return new ListCardBankController().handle(request, response);
 });
 
 export { cardBankRoutes };
