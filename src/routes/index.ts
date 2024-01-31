8c564fe49f32d5f45a499fc684adaf5de7034608
Caio <caiothalles607@gmail.com>
2024-01-31 20:24:01 -0300

    fix(inde-routes): add route for cardBank

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./userRoutes";
 import { accountRoutes } from "./accountRoutes";
+import { cardBankRoutes } from "./cardBankRoutes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/accounts", accountRoutes);
+router.use("/cardBanks", cardBankRoutes);
 
 export { router };
