f13a6ae0ef82f4863a9e60640cba30f7157d5161
Caio <caiothalles607@gmail.com>
2024-01-31 16:50:33 -0300

    fix(accoutsRoutes): route to view balance and transfer route created

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { CreateAccountController } from "../modules/accounts/controllers/createAccountController";
 import { ListAccountsController } from "../modules/accounts/controllers/listAccountsController";
 import { DeleteAccountController } from "../modules/accounts/controllers/deleteAccountController";
+import { CheckAccountBalanceController } from "../modules/accounts/controllers/checkAccountBalanceController";
+import { TransactionController } from "../modules/accounts/controllers/transactionController";
 
 const accountRoutes = Router();
 
@@ -13,7 +15,15 @@ accountRoutes.get("/", (request, response) => {
   return new ListAccountsController().handle(request, response);
 });
 
+accountRoutes.get("/:userId/balance", (request, response) => {
+  return new CheckAccountBalanceController().handle(request, response);
+});
+
 accountRoutes.delete("/", (request, response) => {
   return new DeleteAccountController().handle(request, response);
+});
+
+accountRoutes.patch("/", (request, response) => {
+  return new TransactionController().handle(request, response);
 });
 export { accountRoutes };
