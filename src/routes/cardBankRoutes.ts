7f974fcdd39f60570c29b16e830c600b7a0bc88c
Caio <caiothalles607@gmail.com>
2024-01-31 21:34:55 -0300

    fix(cardBankRoutes): add route for transaction for card

--- src/routes/cardBankRoutes.ts
+++ src/routes/cardBankRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateCardBankController } from "../modules/card_bank/controllers/createCardBankController";
 import { ListCardBankController } from "../modules/card_bank/controllers/listCardBankController";
+import { CardTransactionController } from "../modules/card_bank/controllers/cardTransactionController";
 
 const cardBankRoutes = Router();
 
@@ -10,6 +11,10 @@ cardBankRoutes.post("/", (request, response) => {
 
 cardBankRoutes.get("/", (request, response) => {
   return new ListCardBankController().handle(request, response);
+});
+
+cardBankRoutes.patch("/", (request, response) => {
+  return new CardTransactionController().handle(request, response);
 });
 
 export { cardBankRoutes };
